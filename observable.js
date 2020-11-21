edc49dba015ef717f70030752eb3a9417d81ea41
dokdo2005 <dokdo2005@gmail.com>
2020-11-21 12:05:35 +0900

    add Observable class

---
+++ observable.js
@@ -0,0 +1,17 @@
+export class Observable {
+    constructor() {
+        this.observers = [] ;
+    }
+
+    subscribe(observer) {
+        this.observers = [...this.observers, observer]; 
+    }
+
+    unsubscrive(observer) {
+        this.observers = this.observers.filter(obs => observer !== obs)
+    }
+
+    notify(state) {
+        this.observers.forEach(observer => observer(state));
+    }
+}
