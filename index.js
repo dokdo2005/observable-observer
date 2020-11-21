428f80425b3e5dcf29a17e96962cd2705a5881d6
dokdo2005 <dokdo2005@gmail.com>
2020-11-21 13:29:12 +0900

    add observers

---
+++ index.js
@@ -0,0 +1,69 @@
+// Reference : https://velog.io/@rolled-potatoes/%EC%98%B5%EC%A0%80%EB%B2%84-%ED%8C%A8%ED%84%B4-observer-pattern-with-JavaScript
+
+import {valueSubject} from './valueSubject';
+import {arraySubject} from './arraySubject';
+import {objectSubject} from './objectSubject';
+
+export class valueObserver {
+    constructor() {
+        this.subject = new valueSubject();
+        this.subject.subscribe(this.update.bind(this));
+    }
+
+    update(state) {
+        const {value} = state;
+        console.log(`${typeof value}의 값이 변화하였습니다: ${value}`);
+    }
+
+    changeValue(newValue) {
+        this.subject.changeValue(newValue);
+    }
+}
+
+export class arrayObserver {
+    constructor() {
+        this.subject = new arraySubject();
+        this.subject.subscribe(this.update.bind(this));
+    }
+
+    update(state) {
+        const {value} = state;
+        console.log(`Array가 변화하였습니다: ${value}`);
+    }
+
+    changeIndex(oldIndex, newIndex) {
+        this.subject.changeIndex(oldIndex, newIndex);
+    }
+
+    addItem(item) {
+        this.subject.addItem(item);
+    }
+
+    removeItem(item) {
+        this.subject.removeItem(item);
+    }
+}
+
+export class objectObserver {
+    constructor() {
+        this.subject = new objectSubject();
+        this.subject.subscribe(this.update.bind(this));
+    }
+
+    update(state) {
+        const {value} = state;
+        console.log(`Object가 변화하였습니다: ${value}`);
+    }
+
+    changeValue(property, newValue) {
+        this.subject.changeValue(property, newValue);
+    }
+
+    addProperty(newProperty, newValue) {
+        this.subject.addProperty(newProperty, newValue);
+    }
+
+    removeProperty(deletedProperty) {
+        this.subject.removeProperty(deletedProperty);
+    }
+}
