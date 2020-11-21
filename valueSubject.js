a2de6dc643bd90c350889276c5d877a9d61c0a7c
dokdo2005 <dokdo2005@gmail.com>
2020-11-21 12:05:48 +0900

    add value change subject

---
+++ valueSubject.js
@@ -0,0 +1,20 @@
+import {Observable} from './observable';
+
+export class valueSubject extends Observable {
+    constructor() {
+        super();
+        this.state = {
+            value: undefined
+        }
+    }
+
+    setState(nextState) {
+        this.state = nextState;
+        this.notify(this.state);
+    }
+
+    changeValue(newValue) {
+        const {value} = this.state;
+        this.setState({...this.state, value: newValue});
+    }
+}
