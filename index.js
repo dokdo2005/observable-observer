e01fd8375f781c27d90170fb4a472b035a1dc3d1
dokdo2005 <dokdo2005@gmail.com>
2020-11-22 11:41:42 +0900

    add some use examples

--- index.js
+++ index.js
@@ -1,72 +1,21 @@
 // Reference : https://velog.io/@rolled-potatoes/%EC%98%B5%EC%A0%80%EB%B2%84-%ED%8C%A8%ED%84%B4-observer-pattern-with-JavaScript
 
-import {valueSubject} from './subjects/valueSubject';
-import {arraySubject} from './subjects/arraySubject';
-import {objectSubject} from './subjects/objectSubject';
+import {valueObserver, arrayObserver, objectObserver} from './observable';
 
-export class valueObserver {
-    constructor() {
-        super();
-        this.subject = new valueSubject();
-        this.subject.subscribe(this.update.bind(this));
-    }
+const obs1 = new valueObserver();
+const obs2 = new arrayObserver();
+const obs3 = new objectObserver();
 
-    update(state) {
-        const {value} = state;
-        console.log(`${typeof value}의 값이 변화하였습니다: ${value}`);
-    }
+obs1.changeValue(3);
+obs1.changeValue(4);
+obs1.changeValue(1);
 
-    changeValue(newValue) {
-        this.subject.changeValue(newValue);
-    }
-}
+obs2.addItem(10);
+obs2.addItem(12);
+obs2.changeIndex(0, 1);
+obs2.removeItem(12);
 
-export class arrayObserver {
-    constructor() {
-        super();
-        this.subject = new arraySubject();
-        this.subject.subscribe(this.update.bind(this));
-    }
-
-    update(state) {
-        const {value} = state;
-        console.log(`Array가 변화하였습니다: ${value}`);
-    }
-
-    changeIndex(oldIndex, newIndex) {
-        this.subject.changeIndex(oldIndex, newIndex);
-    }
-
-    addItem(item) {
-        this.subject.addItem(item);
-    }
-
-    removeItem(item) {
-        this.subject.removeItem(item);
-    }
-}
-
-export class objectObserver {
-    constructor() {
-        super();
-        this.subject = new objectSubject();
-        this.subject.subscribe(this.update.bind(this));
-    }
-
-    update(state) {
-        const {value} = state;
-        console.log(`Object가 변화하였습니다: ${value}`);
-    }
-
-    changeValue(property, newValue) {
-        this.subject.changeValue(property, newValue);
-    }
-
-    addProperty(newProperty, newValue) {
-        this.subject.addProperty(newProperty, newValue);
-    }
-
-    removeProperty(deletedProperty) {
-        this.subject.removeProperty(deletedProperty);
-    }
-}
+obs3.addProperty("a", "Hello");
+obs3.addProperty("b", "World");
+obs3.changeValue("b", "Programming World!");
+obs3.removeProperty("a");
---
+++ observers.js
@@ -0,0 +1,70 @@
+import {valueSubject} from './subjects/valueSubject';
+import {arraySubject} from './subjects/arraySubject';
+import {objectSubject} from './subjects/objectSubject';
+
+export class valueObserver {
+    constructor() {
+        super();
+        this.subject = new valueSubject();
+        this.subject.subscribe(this.update.bind(this));
+    }
+
+    update(state) {
+        const {value} = state;
+        console.log(`${typeof value}의 값이 변화하였습니다: ${value}`);
+    }
+
+    changeValue(newValue) {
+        this.subject.changeValue(newValue);
+    }
+}
+
+export class arrayObserver {
+    constructor() {
+        super();
+        this.subject = new arraySubject();
+        this.subject.subscribe(this.update.bind(this));
+    }
+
+    update(state) {
+        const {value} = state;
+        console.log(`Array가 변화하였습니다: ${value}`);
+    }
+
+    changeIndex(oldIndex, newIndex) {
+        this.subject.changeIndex(oldIndex, newIndex);
+    }
+
+    addItem(item) {
+        this.subject.addItem(item);
+    }
+
+    removeItem(item) {
+        this.subject.removeItem(item);
+    }
+}
+
+export class objectObserver {
+    constructor() {
+        super();
+        this.subject = new objectSubject();
+        this.subject.subscribe(this.update.bind(this));
+    }
+
+    update(state) {
+        const {value} = state;
+        console.log(`Object가 변화하였습니다: ${value}`);
+    }
+
+    changeValue(property, newValue) {
+        this.subject.changeValue(property, newValue);
+    }
+
+    addProperty(newProperty, newValue) {
+        this.subject.addProperty(newProperty, newValue);
+    }
+
+    removeProperty(deletedProperty) {
+        this.subject.removeProperty(deletedProperty);
+    }
+}
